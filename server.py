48175b67b6a903d388e19c6620d03f5e9f36e0ed
Alex <coolspammail@gmail.com>
2020-08-04 07:32:32 -0400

    remove proxy

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-"""Сервер Telegram бота, запускаемый непосредственно"""
 import logging
 import os
 
-import aiohttp
+# import aiohttp
 from aiogram import Bot, Dispatcher, executor, types
 
 import exceptions
@@ -13,16 +12,18 @@ from middlewares import AccessMiddleware
 
 logging.basicConfig(level=logging.INFO)
 
-API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
-PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
-PROXY_AUTH = aiohttp.BasicAuth(
-    login=os.getenv("TELEGRAM_PROXY_LOGIN"),
-    password=os.getenv("TELEGRAM_PROXY_PASSWORD")
-)
-ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
+# PROXY_URL = os.getenv("TELEGRAM_PROXY_URL")
+# PROXY_AUTH = aiohttp.BasicAuth(
+#     login=os.getenv("TELEGRAM_PROXY_LOGIN"),
+#     password=os.getenv("TELEGRAM_PROXY_PASSWORD")
+# )
+# bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
 
-bot = Bot(token=API_TOKEN, proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
 dp.middleware.setup(AccessMiddleware(ACCESS_ID))
 
 
